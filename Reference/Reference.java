6e853d8ec672b8ec3c29fcd8b64e34ef31c52934
RedGreenCode <duncan@redgreencode.com>
2015-04-30 22:05:22 -0700

    Added several new reference examples, and Notes.txt for template

--- Reference/Reference.java
+++ Reference/Reference.java
@@ -4,8 +4,8 @@
 //
 // For more information, see http://www.redgreencode.com/about-project-462/
 
-// Lots of useful stuff in here
-import java.util.*;
+import java.util.*;	// Lots of useful stuff in here
+import java.text.DecimalFormat;
 
 // Filename (Reference.java) should match the public class name (Reference)
 public class Reference {
@@ -210,5 +210,123 @@ public class Reference {
 			}
 		}
 		System.out.println(sum);
-	}
-}
+
+		// --> Print the ceiling and floor (space-delimited) of each number in the input
+		// Math.ceil
+		// Math.floor
+		line = r.Next();
+		tokens = line.split(" ");
+		for (String s : tokens) {
+			double d = Double.parseDouble(s);
+			System.out.println(Math.ceil(d) + " " + Math.floor(d));
+		}
+
+		// --> For the next three lines, print true if the first string contains the second string, false otherwise
+		// contains
+		for (int j=0; j<3; j++) {
+			line = r.Next();
+			tokens = line.split(" ");
+			System.out.println(tokens[0].contains(tokens[1]));
+		}
+
+		// --> Print the input with leading and trailing spaces removed
+		// trim
+		line = r.Next();
+		System.out.println(line.trim());
+
+		// --> Convert pipe delimiters to dash delimiters, and end line with a dash
+		// split (pipe)
+		line = r.Next();
+		tokens = line.split("\\|");	// split accepts a regex, so | has to be escaped
+		for (String s : tokens) System.out.print(s + "-");
+		System.out.println();
+
+		// --> Convert the input string to its ASCII values (space-delimited)
+		line = r.Next();
+		for (int j=0; j<line.length(); j++) System.out.print(((int)line.charAt(j)) + " ");
+		System.out.println();
+
+		// --> Assuming the input values represent ASCII values, print the string they represent
+		line = r.Next();
+		tokens = line.split(" ");
+		for (String s : tokens) {
+			int c = Integer.parseInt(s);
+			System.out.print((char)c);
+		}
+		System.out.println();
+
+		// --> Convert the input string to numerical values (space-delimited), where
+		// lowercase a = 0, b = 1, and so on
+		line = r.Next();
+		for (int j=0; j<line.length(); j++) System.out.print((line.charAt(j) - 'a') + " ");
+		System.out.println();
+
+		// --> Assuming the input values represent lowercase letters, where a=0, b=1, etc.,
+		// print the corresponding string
+		line = r.Next();
+		tokens = line.split(" ");
+		for (String s : tokens) {
+			int c = Integer.parseInt(s);
+			System.out.print((char)(c + 'a'));
+		}
+		System.out.println();
+
+		// --> Read whitespace-delimited integers into an array; print them out; create a
+		// reference to the array; modify the first element; print the original array;
+		// create a copy of the array; modify the first element; print the original array.
+		//
+		// clone
+		line = r.Next();
+		tokens = line.trim().split("\\s+");
+		nums = new int[tokens.length];
+		i = 0;
+		for (String str : tokens) nums[i++] = Integer.parseInt(str);
+		for (int n : nums) System.out.print(n + " ");
+		System.out.println();
+		int[] nums2 = nums;	// this is a reference to nums, so changes to nums2 will affect nums
+		nums2[0] = 99;
+		for (int n : nums) System.out.print(n + " ");
+		System.out.println();
+		int[] nums3 = nums.clone();	// this is a copy of nums, so changes to nums3 will NOT affect nums
+		nums3[0] = 0;
+		for (int n : nums) System.out.print(n + " ");
+		System.out.println();
+
+		// --> Initialize an array of integer literals, and print the result
+		int[] literals = new int[] { 9, 8, 7, 6, 5 };
+		for (int n : literals) System.out.print(n + " ");
+		System.out.println();
+
+		// --> Read whitespace-delimited integers into an ArrayList; print whether the array contains 23
+		// contains
+		line = r.Next();
+		tokens = line.trim().split("\\s+");
+		numsList = new ArrayList<Integer>();
+		for (String str : tokens) numsList.add(Integer.parseInt(str));
+		System.out.println(numsList.contains(23));
+
+		// --> Read whitespace-delimited integers into an ArrayList; convert it to an array, and print the result
+		// toArray
+		line = r.Next();
+		tokens = line.trim().split("\\s+");
+		numsList = new ArrayList<Integer>();
+		for (String str : tokens) numsList.add(Integer.parseInt(str));
+		Integer[] numsArr = numsList.toArray(new Integer[numsList.size()]);	// have to use Integer, not int
+		for (int n : numsArr) System.out.print(n + " ");
+		System.out.println();
+
+		// --> Divide the first input by the second input and print the
+		// result to three decimal places
+		//
+		// DecimalFormat
+		line = r.Next();
+		tokens = line.trim().split("\\s+");
+		double n1 = Double.parseDouble(tokens[0]);
+		double n2 = Double.parseDouble(tokens[1]);
+		DecimalFormat df = new DecimalFormat("0.000");
+		System.out.println(df.format(n1/n2));
+
+		// --> Next example goes above this line
+
+	}	// end of public static void main
+}	// end of public class Reference
